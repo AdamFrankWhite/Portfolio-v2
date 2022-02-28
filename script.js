9c934bcba31ca7b892a964225ff24988fbaa111c
Adam White <adamfrankwhite@gmail.com>
2022-02-28 21:56:28 +0000

    add packages and footer

--- script.js
+++ script.js
@@ -3,7 +3,11 @@ const sideMenu = document.getElementById("side-menu");
 
 burgerMenu.addEventListener("click", () => {
     burgerMenu.classList.toggle("is-active");
-    sideMenu.style.width == "100%"
-        ? (sideMenu.style.width = 0)
-        : (sideMenu.style.width = "100%");
+    if (sideMenu.style.width == "100%") {
+        sideMenu.style.width = 0;
+        document.body.style.overflow = "auto";
+    } else {
+        sideMenu.style.width = "100%";
+        document.body.style.overflow = "hidden";
+    }
 });
