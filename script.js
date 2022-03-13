6555b1bacab3a82f7807dd11c3a05300708e67b6
Adam White <adamfrankwhite@gmail.com>
2022-03-13 21:59:30 +0000

    added fixed nav on scroll

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const burgerMenu = document.getElementById("burger-menu");
 const sideMenu = document.getElementById("side-menu");
-
+const header = document.getElementsByTagName("header")[0];
+const heroContainer = document.getElementsByClassName("hero-cont")[0];
 burgerMenu.addEventListener("click", () => {
     burgerMenu.classList.toggle("is-active");
     if (sideMenu.style.width == "100%") {
@@ -11,3 +12,21 @@ burgerMenu.addEventListener("click", () => {
         document.body.style.overflow = "hidden";
     }
 });
+
+// Sticky header
+const sentinelEl = document.getElementById("sentinel");
+const stuckClass = "fixed";
+
+const handler = (entries) => {
+    if (header) {
+        if (!entries[0].isIntersecting) {
+            header.classList.add(stuckClass, "slide-down");
+            heroContainer.classList.add("pt"); // padding to avoid body jumping up
+        }
+    }
+};
+let options = {
+    rootMargin: "100px",
+};
+const observer = new window.IntersectionObserver(handler, options);
+observer.observe(sentinelEl);
